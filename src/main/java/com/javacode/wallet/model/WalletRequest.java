480ed40d77de665ae1660a42712a707a985dcfd1
Andrew <andfrewluferenko@icloud.com>
2024-09-17 11:55:29 +0300

    created DTO to wallet

---
+++ src/main/java/com/javacode/wallet/model/WalletRequest.java
@@ -0,0 +1,24 @@
+package com.javacode.wallet.model;
+
+import jakarta.persistence.Version;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@RequiredArgsConstructor
+@Accessors(chain = true)
+public class WalletRequest {
+
+    private UUID walletId;
+
+    private OperationType operationType;
+
+    private BigDecimal amount;
+//
+//    @Version
+//    private Long version;
+}
